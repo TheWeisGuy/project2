ca1cb055cdcc5192cc7203b9343732280986264e
TheWeisGuy <henryweisman14@gmail.com>
2021-10-13 14:18:41 -0400

    Added parser.java

--- parser.java
+++ parser.java
@@ -6,14 +6,15 @@ import java.io.IOException;
 import java.io.*;
 
 
-public class parser {
+public class Parser {
     private ArrayList<String> input;
-    private String output;
+    private ArrayList<String> output;
     private String filePath;
-    public parser(String fileName){
-        this.output = "";
+    public Parser(String fileName){
         this.filePath = fileName;
         this.input = this.readFile();
+        System.out.println(this.tokenize().get(0)[0]);
+        this.output = this.toPostfix(this.tokenize());
     }
 
     /**
@@ -52,7 +53,7 @@ public class parser {
         ArrayList<String[]> out = new ArrayList<String[]>();
 
         //Loops through each line
-        for(int i = 0; i < out.size(); i++){
+        for(int i = 0; i < this.input.size(); i++){
 
             String line = this.input.get(i);
             //Spliting by spaces in the line
@@ -64,8 +65,52 @@ public class parser {
         return out;
     }
 
-    
-    
+    /**
+     * @param tokens ArrayList of token strings
+     * @return ArrayList of outfix strings
+     */
+    private ArrayList<String> toPostfix(ArrayList<String[]> tokens){
+        Stack<String> stack = new Stack<String>();
+        ArrayList<String> out = new ArrayList<String>();
+        for(int i = 0; i<tokens.size();i++){
+            String[] line = tokens.get(i);
+            String token = line[0];
+            int j = 1;
 
+            //crashes here
+            //while loop does not meet end condition
+            while(token!=";"){
+                if(token == ")"){
+                    String right = stack.pop();
+                    String op = stack.pop();
+                    String left = stack.pop();
+                    stack.push(left+right+op);
+                }
+                else if(token!="("){
+                    stack.push(token);
+                }
+                System.out.println(token);
+                token = line[j];
+                j++;
+                
+            }
+            if(stack.getLength()>1){
+                System.out.println("Invalid Infix expression at line: "+Integer.toString(i+1));
+            }
+            else{
+                out.add(stack.pop());
+            }
+        }
+        return(out);
+    }
+
+    public ArrayList<String> getOutput(){
+        return(this.output);
+    }
+
+    public static void main(String argv[]){
+        Parser parse = new Parser(argv[0]);
+        System.out.println(parse.getOutput().toString());
+    }
 
 }
