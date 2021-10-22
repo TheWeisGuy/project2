8ca8184354cc028c494f917bd7063ebf9abadb24
Oreo-Monster <ewrong16@gmail.com>
2021-10-22 18:06:26 -0400

    closed #4 : Moved relavant code from Parser.java to Postfix.java, added command line functionality

--- Postfix.java
+++ Postfix.java
@@ -1,26 +1,25 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.*;
+/*
+Henry Weisman & Emerson Wright
+CS 321 Project 2
 
-/*This class reads in infix expressions and 
- *Outputs equivilent assembly code
- *
- *Takes the path of the input file, which may contain
- * multiple infix expressions on differnt lines separated by ";"
- *
- * */
-public class Parser {
-    //input and output store all the infix and assembly/outfix respectivly
+This class will take an input file with infix expressions
+If an output file is specified, then it will write all
+postfix expressions there, otherwise it is printed
+
+Usage:
+java Postfix <input> <output>
+*/
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Postfix {
     private ArrayList<String> input;
     private ArrayList<String> output;
     private String filePath;
-
-    //Takes the file path fo the input file and will write to an output file
-    //NOTE: What is the output file?
-    public Parser(String fileName){
+    
+        
+    public Postfix(String fileName){
         this.filePath = fileName;
         this.input = this.readFile();
         this.output = this.toPostfix(this.tokenize());
@@ -30,19 +29,18 @@ public class Parser {
      * reades file and returns list of strings of lines
      * @return ArrayList of each line
      */
-    private ArrayList<String> readFile(){
+    public ArrayList<String> readFile(){
         try{
-            //Reading in file and putting each line in ArrayList
             FileReader fr = new FileReader(new File(this.filePath));
             BufferedReader f = new BufferedReader(fr);
             ArrayList<String> strings = new ArrayList<String>();
             String line = f.readLine();
-            strings.add(line);
-            while(line==null){
+            while(line!=null){
                 strings.add(line);
+                line = f.readLine();
             }
             f.close();
-            fr.close();
+            this.input = strings;
             return strings;
         }
         catch(FileNotFoundException ex) {
@@ -58,6 +56,8 @@ public class Parser {
     Splits lines by spaces
     */
     private ArrayList<String[]> tokenize(){
+
+
         //Stores the arrays of tokens
         ArrayList<String[]> out = new ArrayList<String[]>();
 
@@ -66,7 +66,6 @@ public class Parser {
 
             String line = this.input.get(i);
             //Spliting by spaces in the line
-            //NOTE: Unsure if the \n is also added to tokens, may cause problems
             String[] tokens = line.split(" ");
             out.add(tokens);
         }
@@ -80,31 +79,26 @@ public class Parser {
      */
     private ArrayList<String> toPostfix(ArrayList<String[]> tokens){
         Stack<String> stack = new Stack<String>();
-        //out will hold a list of all infix expressions
         ArrayList<String> out = new ArrayList<String>();
-        //loops throught all the tokenized infix expressions
         for(int i = 0; i<tokens.size();i++){
-            //Getting the current infix
             String[] line = tokens.get(i);
-            //initiating loop varables
             String token = line[0];
             int j = 1;
-            while(!token.equals(";")){
+
+            while(!(token.equals(";"))){
                 if(token.equals(")")){
-                    //end of operation, pushing operation onto stack
                     String right = stack.pop();
                     String op = stack.pop();
                     String left = stack.pop();
-                    stack.push(String.format("%s %s %s", left, right, op));
+                    stack.push(left+" "+right+" "+op);
                 }
-                else if(!token.equals("(")){
+                else if(!(token.equals("("))){
                     stack.push(token);
                 }
-                token = line[j].strip();
+                token = line[j];
                 j++;
                 
             }
-            //For help debugging, the stack should always have a size of 1
             if(stack.getLength()>1){
                 System.out.println("Invalid Infix expression at line: "+Integer.toString(i+1));
             }
@@ -115,14 +109,35 @@ public class Parser {
         return(out);
     }
 
-    //returns outputs
     public ArrayList<String> getOutput(){
         return(this.output);
     }
 
     public static void main(String argv[]){
-        Parser parse = new Parser(argv[0]);
-        System.out.println(parse.getOutput().toString());
+        Postfix p = new Postfix(argv[0]);
+        ArrayList<String> out = p.getOutput();
+        String str = "";
+        for(int i = 0; i < out.size(); i++ ){
+            str += out.get(i);
+            str += "\n";
+        }
+
+
+        if (argv.length > 1){
+            String path = argv[1];
+            File f = new File(path);
+            try{
+                f.createNewFile();
+                FileWriter w = new FileWriter(path);
+                w.write(str);
+                w.close();
+            }catch (IOException e){
+                System.out.println("error writing output to given path");
+            }
+        }else{
+            System.out.println(str);
+        }
+
     }
 
 }
